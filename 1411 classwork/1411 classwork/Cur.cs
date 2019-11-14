8f0bea23f720e78ffa12f00109f53a9af434262a
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2019-11-14 20:34:41 +0200

    Add files via upload

---
+++ 1411 classwork/1411 classwork/Cur.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1411_classwork
+{
+    public class Car
+    {
+        public string model;
+        public int year;
+        public string color;
+        public float height;
+        public bool work;
+
+        public Car()
+        {
+            this.model = "no model";
+            this.year = -1;
+            this.color = "no color";
+            this.height = -1.0f;
+            this.work = true;
+        }
+
+        public Car(string model)
+        {
+            this.model = model;
+            this.year = -1;
+            this.color = "no color";
+            this.height = -1.0f;
+            this.work = true;
+        }
+
+        public Car(string model, int year, string color, float height, bool work)
+        {
+            this.model = model;
+            this.year = year;
+            this.color = color;
+            this.height = height;
+            this.work = work;
+        }
+
+        public void Fix()
+        {
+            this.work = true;
+        }
+
+        public void Paint(string color)
+        {
+            this.color = color;
+        }
+
+        public void BadTire()
+        {
+            this.work = false;
+            this.height = this.height * 0.8f;
+        }
+
+        public override string ToString()
+        {
+            return $"Cur: model: {model}, year: {year}, color: {color}, height: {height}, work:{work}";
+        }
+    }
+}
---
+++ 1411 classwork/1411 classwork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1411_classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Car car2 = new Car("mazda");
+            Car car3 = new Car("volvo", 1999, "purple", 1.8f, true);
+
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);
+            Console.WriteLine(car3);
+        }
+    }
+}
